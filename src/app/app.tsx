5feb29b154bd9d354b9f1c332b4a5bacff4c805b
fuzzywombatt <ryangibson365@gmail.com>
2023-06-30 18:39:46 -0500

    continue setup with nx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import React from 'react'
 
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
+      Federated to the gills {React.version}
       <NxWelcome title="client" />
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
+const { merge } = require('webpack-merge')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPLugin')
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
-  // Update the webpack config as needed here.
-  // e.g. `config.plugins.push(new MyPlugin())`
-  return config;
+
+  return merge(config, {
+    output: {
+      uniqueName: 'client',
+      publicPath: 'auto',
+      scriptType: 'text/javascript'
+    }
+  })
 });
